1a4171760b6fe83f1d3ac455fafb2ea8e3fcefcb
Chris Butterfield <johbt@amazon.com>
2018-10-22 15:12:46 -0700

    Added test bot

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+import os
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+bot.run(os.environ['DISCORD_TOKEN'])
