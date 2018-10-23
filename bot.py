96e4b45d4e3b53696454629a5c0ba874370cdbdc
Chris Butterfield <johbt@amazon.com>
2018-10-22 18:34:15 -0700

    test flask

--- bot.py
+++ bot.py
@@ -1,18 +1,50 @@
-import discord
-import os
-from discord.ext import commands
+# import asyncio
+# import discord
+# import os
+# from flask import Flask
+# from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+# from pylexa.app import alexa_blueprint
+# from pylexa.intent import handle_intent
+# from pylexa.response import PlainTextSpeech
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+# app = Flask(__name__)
+# app.config['app_id'] = os.getenv('ALEXA_APP_ID')
+# app.register_blueprint(alexa_blueprint)
 
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
+# loop = asyncio.get_event_loop()
+# bot = commands.Bot(loop=loop, command_prefix="!")
 
-bot.run(os.environ['DISCORD_TOKEN'])
+# @bot.event
+# async def on_ready():
+#     print('Logged in as')
+#     print(bot.user.name)
+#     print(bot.user.id)
+#     print('------')
+
+# @bot.command()
+# async def greet(ctx):
+#     await ctx.send(":smiley: :wave: Hello, there!")
+
+# app.
+# bot.run(os.environ['DISCORD_TOKEN'])
+
+
+# @handle_intent('Echo')
+# def handle_echo_intent(request):
+#     return PlainTextSpeech(
+#         request.slots.get('message', 'Nothing to echo'))
+
+from flask import Flask, render_template
+from flask_ask import Ask, statement
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.intent('GetSongIntent')
+def hello(firstname):
+    text = render_template('hello', firstname=firstname)
+    return statement(text).simple_card('Hello', text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
