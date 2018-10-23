92eb345e011336317add3faf86b48bc6e09b4bd5
Chris Butterfield <johbt@amazon.com>
2018-10-23 12:08:22 -0700

    ajfwoe

--- bot.py
+++ bot.py
@@ -3,19 +3,19 @@ import discord
 import os
 # from flask import Flask
 # from flask_ask import Ask, statement
+from discord import opus
 from discord.ext import commands
 import urllib
 from bs4 import BeautifulSoup
 
-
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
-
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+if not opus.is_loaded():
+    try:
+        opus.load_opus(opus_lib)
+        return
+    except OSError:
+        pass
+    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
     
 # app = Flask(__name__)    
 # ask = Ask(app, '/')
