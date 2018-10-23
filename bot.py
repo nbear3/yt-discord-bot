832f2f647434e28b751e397527e66aed4ed01cb1
Chris Butterfield <johbt@amazon.com>
2018-10-23 00:28:22 -0700

    test discord

--- bot.py
+++ bot.py
@@ -1,31 +1,61 @@
 import asyncio
 import discord
 import os
-from flask import Flask, render_template
-from flask_ask import Ask, statement
+# from flask import Flask
+# from flask_ask import Ask, statement
 from discord.ext import commands
 import urllib
 from bs4 import BeautifulSoup
-import threading
 
-app = Flask(__name__)
-ask = Ask(app, '/')
-loop = asyncio.new_event_loop()
-bot = commands.Bot(loop=loop, command_prefix="!")
-global_ctx = {}
 
-@bot.event
+# app = Flask(__name__)    
+# ask = Ask(app, '/')
+bot = commands.Bot(command_prefix="!")
+# global_ctx = {}
+
+# @bot.event
+# async def on_ready():
+#     print('Logged in as')
+#     print(bot.user.name)
+#     print(bot.user.id)
+#     print('------')
+#     print(search(global_ctx['song']))
+#     await bot.close()
+
+def search(search_query):
+    # query = urllib.quote(search_query)
+    url = "https://www.youtube.com/results?search_query=" + search_query
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    vid = soup.find(attrs={'class':'yt-uix-tile-link'})
+    return 'https://www.youtube.com' + vid['href']
+
+# @ask.intent('GetSongIntent')
+# def getSong(song):
+#     global_ctx['song'] = song
+#     run_bot()
+#     return statement("Playing %s" % song).simple_card('Now Playing', song)
+
+
+@client.command(name='play',
+                pass_context=True,
+                description='play from youtube',
+                brief='URL',
+                aliases=['p'])
+async def play_song(ctx, *search_query):
+    url = search('+'.join(search_query).strip())
+    voice = await client.join_voice_channel(global_player['bear'].voice.voice_channel)
+    player = await voice.create_ytdl_player(url)
+    global_player['yt'] = player
+    player.start()
+
+
+@client.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print('Logged in as %s' % client.user.name)
+    print(client.user.id)
     print('------')
-    print(search(global_ctx['song']))
-    await bot.close()
-
-
-def run_bot():
-    loop.run_until_complete(bot.run(os.environ['DISCORD_TOKEN']))
 
 
 def search(search_query):
@@ -38,9 +68,5 @@ def search(search_query):
     return 'https://www.youtube.com' + vid['href']
 
 
-@ask.intent('GetSongIntent')
-def getSong(song):
-    global_ctx['song'] = song
-    run_bot()
-    return statement("Playing %s" % song).simple_card('Now Playing', song)
+bot.run(os.environ['DISCORD_TOKEN'])
 
