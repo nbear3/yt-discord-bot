b763bb8f3dc7c546867c4d4228d349738f5a7fa4
Chris Butterfield <johbt@amazon.com>
2018-10-22 19:34:47 -0700

    test 5

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
 import asyncio
 import discord
 import os
-# from flask import Flask
+from flask import Flask, render_template
+from flask_ask import Ask, statement
 from discord.ext import commands
-
-# from pylexa.app import alexa_blueprint
-# from pylexa.intent import handle_intent
-# from pylexa.response import PlainTextSpeech
-
-# app = Flask(__name__)
-# app.config['app_id'] = os.getenv('ALEXA_APP_ID')
-# app.register_blueprint(alexa_blueprint)
+import urllib
+from bs4 import BeautifulSoup
 
 loop = asyncio.get_event_loop()
-bot = commands.Bot(loop=loop, command_prefix="!")
+app = Flask(__name__)
+ask = Ask(app, '/')
+bot = commands.Bot(command_prefix="!")
+global_ctx = {}
 
 @bot.event
 async def on_ready():
@@ -21,26 +19,22 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    bot.disconnect()
+    print(search(global_ctx['song']))
+    bot.close()
 
-# @bot.command()
-# async def greet(ctx):
-#     await ctx.send(":smiley: :wave: Hello, there!")
-
-# app.
-# bot.run(os.environ['DISCORD_TOKEN'])
-
-from flask import Flask, render_template
-from flask_ask import Ask, statement
-
-loop = asyncio.get_event_loop()
-app = Flask(__name__)
-ask = Ask(app, '/')
+def search(search_query):
+    # query = urllib.quote(search_query)
+    url = "https://www.youtube.com/results?search_query=" + search_query
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    vid = soup.find(attrs={'class':'yt-uix-tile-link'})
+    return 'https://www.youtube.com' + vid['href']
 
 @ask.intent('GetSongIntent')
 def getSong(song):
-    text = song
-    loop.run_until_complete(bot.run(os.environ['DISCORD_TOKEN']))
+    global_ctx['song'] = song
+    loop.ensure_future(bot.run(os.environ['DISCORD_TOKEN']))
     return statement(text).simple_card('Hello', text)
 
 # if __name__ == '__main__':
