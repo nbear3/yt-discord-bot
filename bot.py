f8b5c01db83a432cd75f428f091f7cba469198dc
Chris Butterfield <johbt@amazon.com>
2018-10-22 19:25:32 -0700

    add discord bot

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-# import asyncio
-# import discord
-# import os
+import asyncio
+import discord
+import os
 # from flask import Flask
-# from discord.ext import commands
+from discord.ext import commands
 
 # from pylexa.app import alexa_blueprint
 # from pylexa.intent import handle_intent
@@ -12,15 +12,16 @@
 # app.config['app_id'] = os.getenv('ALEXA_APP_ID')
 # app.register_blueprint(alexa_blueprint)
 
-# loop = asyncio.get_event_loop()
-# bot = commands.Bot(loop=loop, command_prefix="!")
+loop = asyncio.get_event_loop()
+bot = commands.Bot(loop=loop, command_prefix="!")
 
-# @bot.event
-# async def on_ready():
-#     print('Logged in as')
-#     print(bot.user.name)
-#     print(bot.user.id)
-#     print('------')
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    bot.disconnect()
 
 # @bot.command()
 # async def greet(ctx):
@@ -29,22 +30,18 @@
 # app.
 # bot.run(os.environ['DISCORD_TOKEN'])
 
-
-# @handle_intent('Echo')
-# def handle_echo_intent(request):
-#     return PlainTextSpeech(
-#         request.slots.get('message', 'Nothing to echo'))
-
 from flask import Flask, render_template
 from flask_ask import Ask, statement
 
+loop = asyncio.get_event_loop()
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 @ask.intent('GetSongIntent')
 def getSong(song):
-	text = song
-	return statement(text).simple_card('Hello', text)
+    text = song
+    loop.run_until_complete(bot.run(os.environ['DISCORD_TOKEN']))
+    return statement(text).simple_card('Hello', text)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
